405ff0f51506cd45dff7747db02a6d1724a0738b
fabiojrfla <fabiojrfla@gmail.com>
2022-05-19 16:13:55 -0300

    Adiciona tela inicial da aplicação

---
+++ spec/system/visitor_view_homepage_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Visitante acessa tela inicial' do
+  it 'e vê o nome da aplicação' do
+    visit root_path
+
+    within('h1') do
+      expect(page).to have_content 'Sistema de Frete'
+    end
+  end
+
+  it 'e vê menu de navegação' do
+    visit root_path
+
+    within('nav') do
+      expect(page).to have_content 'Página inicial'
+      expect(page).to have_content 'Rastreamento'
+      expect(page).to have_content 'Entrar'
+    end
+  end
+end
