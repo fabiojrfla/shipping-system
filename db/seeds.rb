4645fe8d9ccc3c96247d387938f18e972b89d944
fabiojrfla <fabiojrfla@gmail.com>
2022-05-26 04:34:37 -0300

    Adiciona validações de Preço mínimo de entrega

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,4 @@ ShippingPrice.create!(start_volume: 50, end_volume: 100, start_weight: 10, end_w
                       price_km: 2, shipping_company: second_sc)
 
 MinShippingPrice.create!(start_distance: 0, end_distance: 100, price: 100, shipping_company: first_sc)
+MinShippingPrice.create!(start_distance: 100, end_distance: 200, price: 150, shipping_company: second_sc)
--- spec/system/prices/user_register_minimum_price_spec.rb
+++ spec/system/prices/user_register_minimum_price_spec.rb
@@ -32,4 +32,54 @@ describe 'Usuário cadastra preços mínimos' do
       expect(page).to have_content 'R$ 100,00'
     end
   end
+
+  it 'com dados inválidos' do
+    ShippingCompany.create!(registration_number: '98765432000198', corporate_name: 'Light Transportes LTDA',
+                            brand_name: 'TransLight', email: 'contato@translight.com.br',
+                            street_name: 'Avenida Alberto Maranhão', street_number: '100', complement: 'Galpão 10',
+                            district: 'Centro', city: 'Mossoró', state: 'RN', postal_code: '59000000')
+    user = User.create!(name: 'Walter', surname: 'White', email: 'walter@translight.com.br', password: 'saymyname')
+
+    login_as(user, scope: :user)
+    visit user_root_path
+    click_on 'Tabela de Preços'
+    within('section#minimum-prices') do
+      click_on 'Novo Preço'
+    end
+    fill_in 'Distância Inicial (km)', with: ''
+    fill_in 'Distância Final (km)', with: '100'
+    fill_in 'Valor (R$)', with: ''
+    click_on 'Criar Preço mínimo'
+
+    expect(page).to have_content 'Dados inválidos...'
+    expect(page).to have_content 'Distância Inicial (km) não pode ficar em branco'
+    expect(page).to have_content 'Valor (R$) não pode ficar em branco'
+
+    expect(page).not_to have_content 'Distância Final (km) não pode ficar em branco'
+
+    expect(page).to have_field 'Distância Final (km)', with: '100'
+  end
+
+  it 'com intervalos já contemplados' do
+    sc = ShippingCompany.create!(registration_number: '98765432000198', corporate_name: 'Light Transportes LTDA',
+                            brand_name: 'TransLight', email: 'contato@translight.com.br',
+                            street_name: 'Avenida Alberto Maranhão', street_number: '100', complement: 'Galpão 10',
+                            district: 'Centro', city: 'Mossoró', state: 'RN', postal_code: '59000000')
+    user = User.create!(name: 'Walter', surname: 'White', email: 'walter@translight.com.br', password: 'saymyname')
+    MinShippingPrice.create!(start_distance: 0, end_distance: 100, price: 100, shipping_company: sc)
+
+    login_as(user, scope: :user)
+    visit user_root_path
+    click_on 'Tabela de Preços'
+    within('section#minimum-prices') do
+      click_on 'Novo Preço'
+    end
+    fill_in 'Distância Inicial (km)', with: '50'
+    fill_in 'Distância Final (km)', with: '150'
+    fill_in 'Valor (R$)', with: '150'
+    click_on 'Criar Preço mínimo'
+
+    expect(page).to have_content 'Dados inválidos...'
+    expect(page).to have_content 'Distância Inicial (km) já inclusa em intervalo cadastrado'
+  end
 end
